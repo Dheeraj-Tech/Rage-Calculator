7d9034b304ac6a4b71439b512165a94691c0847a
Dheeraj-Tech <68982558+Dheeraj-Tech@users.noreply.github.com>
2020-09-08 09:53:50 +0530

    V 1.01.A
    This is our First Version with 4 Possible Operations -> Addition 
                                                                                          Subtraction 
                                                                                          Multiplication
                                                                                          Divison
    Happy Coding,
    Dheeraj

---
+++ Version 1.01.A/RageCalculator1.01.A.py
@@ -0,0 +1,39 @@
+#Prefixes
+print('''            |||||||||||||||||||||||||||||||||||||||||||||||||Welcome To Rage Calculator||||||||||||||||||||||||||||||||||||||||||||||
+                                                                                                                                                Version - 1.01.A''')
+#The types of operations
+print('''
+All the Possible Operations :
+1} Addition(+)
+2}Subtraction(-)
+3}Multiplication(*)
+4}Division(/)
+
+''')
+
+#Program For The Calculator
+a = int(input("Enter The First Number "))
+b = int(input("Enter The Second Number "))
+c = input("Enter The Operation You want to do ")
+if c == "+"or c == "Addition"or c == "ADDITION"or c == "addition"or c == "Add"or c == "ADD"or c == "add" :
+        print(a+b)
+
+elif c == "-"or c == "Subtraction"or c == "SUBTRACTION"or c == "subtarction"or c == "Subtract"or c == "SUBTRACT"or c == "subtract" :
+      print(a-b)
+      print ("If your value is opposite,then try interchanging your value")
+      
+elif c == "*"or c == "Multiplication"or c == "MULTIPLICATION"or c == "multiplication"or c == "Multiply"or c == "MULTIPLY"or c == "multiply" :
+      print(a*b)
+      
+elif c == "/"or c == "Division"or c == "DIVISION"or c == "division"or c == "Division"or c == "DIVISION"or c == "division" :
+      print(a/b)
+      print("If your value is opposite,then try interchanging your value")
+      
+else :
+        print("The following Operation is Invalid")
+#For Developers,
+# Dear Coders, This is not a Copyright Version and hereby allow to use this programming  for further use.
+#Happy Coding,
+#Dheeraj,
+#13
+"""-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"""        
